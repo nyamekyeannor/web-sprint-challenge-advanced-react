4c943b0e4e4928d8d86405f8ee6f7ab3aa6520eb
Nyamekye Annor <nyamekyeannor@Nyamekyes-MacBook-Pro.local>
2021-12-03 11:33:51 -0600

    still working to meet MVP

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,6 +1,6 @@
 import React from "react";
-import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import MutationObserver from "mutationobserver-shim";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -8,3 +8,11 @@ import CheckoutForm from "./CheckoutForm";
 test("renders without errors", () => {});
 
 test("shows success message on submit with form details", () => {});
+
+const firstNameInput = screen.getByLabelText(/first name/i);
+const lastNameInput = screen.getByLabelText(/last name/i);
+const addressInput = screen.getByLabelText(/address/i);
+const cityInput = screen.getByLabelText(/city/i);
+const stateInput = screen.getByLabelText(/state/i);
+const zipInput = screen.getByLabelText(/zip/i);
+const submitButton = screen.getByRole("button", { name: /checkout/i });
